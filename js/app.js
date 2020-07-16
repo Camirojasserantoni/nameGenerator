004851a99eb9c30645e62efc3f4af599c78c57cf
Camila Rojas S <56646322+Camirojasserantoni@users.noreply.github.com>
2020-07-16 15:12:31 -0400

    adding some variables to the generator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-document.querySelector("generate-name");
+document.querySelector("#generate-name").addEventListener('submit', chargeNames);
+
+//call to ajax and print the results 
+
+function chargeNames(e) {
+    e.preventDefault();
+
+
+    //reading
+    const origin = document.getElementById('origin'); 
+    const originSelected = origin.options[origin.selectedIndex].value;
+
+    console.log(originSelected);
+
+
+    const gender = document.getElementById('gender'); 
+    const genderSelected = gender.options[gender.selectedIndex].value;
+
+    console.log(genderSelected);
+
+    const number = document.getElementById('number').value; 
+  
+
+
+    console.log(number);
+
+    let url=''
+    url += 'https://uinames.com/api/?';
+}
